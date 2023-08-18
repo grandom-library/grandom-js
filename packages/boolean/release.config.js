8661c308ef0de7f5eaca825c5879a29c897e6eea
Richard King <richrdkng@gmail.com>
2023-08-18 12:54:36 +0200

    ci(boolean): enable publish to NPM

--- packages/boolean/release.config.js
+++ packages/boolean/release.config.js
@@ -17,14 +17,14 @@ module.exports = {
     ['@semantic-release/exec', {
       // eslint-disable-next-line no-template-curly-in-string
       prepareCmd: 'export SEMANTIC_RELEASE_NEXT_RELEASE_VERSION=${nextRelease.version} && npm run build'
-    }]
+    }],
     // ['@semantic-release/git', {
     //   assets: [
     //     'CHANGELOG*',
     //   ],
     // }],
-    // ['@semantic-release/npm', {
-    //   pkgRoot: 'dist',
-    // }],
+    ['@semantic-release/npm', {
+      pkgRoot: 'dist'
+    }]
   ]
 }
