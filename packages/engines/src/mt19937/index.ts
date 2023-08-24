336e6a6273d4fe933bde7a0585dd38995f06b00c
Richard King <richrdkng@gmail.com>
2023-08-24 20:06:27 +0200

    feat(engines): add seed capability to MT19937Engine
    Add seed getter/setter to MT19937Engine (SeedableEngine) with number and
    string seed capabilities.

--- packages/engines/src/mt19937/index.ts
+++ packages/engines/src/mt19937/index.ts
@@ -1,13 +1,34 @@
 import { RandomEngine } from '@grandom/core'
 import MT19937 from '@grandom/mt19937'
+import fnv1a from '@grandom/fnv1a'
 
 export default class MT19937Engine extends RandomEngine {
   private readonly _engine: MT19937
+  private _seed: any
 
   constructor () {
     super('mt19937')
 
-    this._engine = new MT19937(Math.floor(Math.random() * new Date().getTime()))
+    this._engine = new MT19937()
+    this.seed = Math.floor(Math.random() * new Date().getTime())
+  }
+
+  get seed (): any {
+    return this._seed
+  }
+
+  set seed (seed: any) {
+    if (typeof seed === 'number') {
+      this._engine.seed = seed
+      this._seed = seed
+    } else if (typeof seed === 'string') {
+      this._engine.seed = fnv1a(seed)
+      this._seed = seed
+    } else {
+      throw new TypeError(
+        `Seed must be a number, or a string, got: ${seed} (typeof === '${typeof seed}').`
+      )
+    }
   }
 
   _next (): number {
---
+++ packages/engines/test/mt19937/seed.test.ts
@@ -0,0 +1,55 @@
+import MT19937Engine from '../../src/mt19937'
+
+describe('MT19937Engine', () => {
+  describe('set/get seed', () => {
+    test('auto seed', () => {
+      const engine = new MT19937Engine()
+
+      expect(engine.seed).toBeNumber()
+      expect(engine.seed).toBeWithin(0, Number.MAX_SAFE_INTEGER)
+    })
+
+    describe('set seed', () => {
+      test('set seed - number', () => {
+        const engine = new MT19937Engine()
+
+        engine.seed = 42
+
+        expect(engine.seed).toBe(42)
+        expect(engine.nextInteger()).toBe(1608637542)
+        expect(engine.nextInteger()).toBe(3421126067)
+        expect(engine.nextInteger()).toBe(4083286876)
+        expect(engine.nextInteger()).toBe(787846414)
+        expect(engine.nextInteger()).toBe(3143890026)
+
+        engine.seed = 123456789
+
+        expect(engine.seed).toBe(123456789)
+        expect(engine.nextInteger()).toBe(2288500408)
+        expect(engine.nextInteger()).toBe(4254805660)
+        expect(engine.nextInteger()).toBe(2294099250)
+        expect(engine.nextInteger()).toBe(56498137)
+        expect(engine.nextInteger()).toBe(2188513626)
+      })
+
+      test('set seed - string', () => {
+        const engine = new MT19937Engine()
+
+        engine.seed = '42'
+
+        expect(engine.seed).toBeString()
+        expect(engine.seed).toBe('42')
+      })
+
+      test('set seed - unsupported type', () => {
+        const engine = new MT19937Engine()
+
+        expect(() => engine.seed = false).toThrowWithMessage(
+          TypeError,
+          'Seed must be a number, or a string, got: false (typeof === \'boolean\').'
+        )
+      })
+    })
+  })
+})
+
