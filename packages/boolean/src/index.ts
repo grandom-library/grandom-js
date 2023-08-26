a3af43fd4b85bc9857f5ecf21f66dc2afba3ae12
Richard King <richrdkng@gmail.com>
2023-08-26 19:11:25 +0200

    fix(boolean): fix unaccessible RandomBoolean class in UMD

--- packages/boolean/src/index.ts
+++ packages/boolean/src/index.ts
@@ -4,4 +4,8 @@ import RandomBoolean from './RandomBoolean'
 const random = new RandomBoolean(new BasicEngine())
 const boolean = random.boolean.bind(random)
 
+// @ts-expect-error
+// make RandomBoolean accessible in UMD
+boolean.RandomBoolean = RandomBoolean
+
 export default boolean
