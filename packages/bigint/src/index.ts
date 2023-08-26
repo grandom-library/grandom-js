c261a4b8b0440e617a2396f3dd43b9fab44cc2a3
Richard King <richrdkng@gmail.com>
2023-08-26 21:12:51 +0200

    fix(bigint): make RandomBigInt accessible in UMD

--- packages/bigint/src/index.ts
+++ packages/bigint/src/index.ts
@@ -4,4 +4,8 @@ import RandomBigInt from './RandomBigInt'
 const random = new RandomBigInt(new BasicEngine())
 const bigint = random.bigint.bind(random)
 
+// @ts-expect-error
+// make RandomBigInt accessible in UMD
+bigint.RandomBigInt = RandomBigInt
+
 export default bigint
