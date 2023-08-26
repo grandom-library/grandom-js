328497f9191640b4cf415e2fdeddd314b93138b0
Richard King <richrdkng@gmail.com>
2023-08-26 21:45:32 +0200

    fix(core): trigger release

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,8 +1,4 @@
-const dedent = require('string-dedent')
 const typescript = require('@rollup/plugin-typescript')
-// const terser = require('@rollup/plugin-terser')
-
-const pkg = require('./package.json')
 
 const version = process.env.SEMANTIC_RELEASE_NEXT_RELEASE_VERSION
 
@@ -10,35 +6,18 @@ if (!version) {
   throw new Error(`Invalid version, got: "${version}".`)
 }
 
-const banner = dedent`
-  /*!
-   * ${pkg.name} v${version}
-   * ${pkg.homepage}
-   *
-   * Copyright (c) 2023 ${pkg.author}
-   * Released under the ${pkg.license} License
-   * ${pkg.homepage}/blob/main/LICENSE
-   *
-   * Date: ${new Date().toISOString()}
-   */
-`
-
 /** @type {import('rollup').RollupOptions} */
 module.exports = {
   input: 'src/index.ts',
 
   output: {
-    banner,
-    sourcemap: true,
-
     format: 'cjs',
+    sourcemap: true,
     preserveModules: true,
-
     dir: 'dist'
   },
 
   plugins: [
     typescript()
-    // terser(),
   ]
 }
