6b2e1b34658b9af4eee42d5c1cd9e9bd374805db
Richard King <richrdkng@gmail.com>
2023-08-27 10:05:00 +0200

    build(weighted): move UMD build to dist root

--- packages/weighted/rollup.config.js
+++ packages/weighted/rollup.config.js
@@ -1,44 +1,62 @@
-const dedent = require('string-dedent')
 const typescript = require('@rollup/plugin-typescript')
-// const terser = require('@rollup/plugin-terser')
+const terser = require('@rollup/plugin-terser')
+const commonjs = require('@rollup/plugin-commonjs')
+const nodeResolve = require('@rollup/plugin-node-resolve')
+const dedent = require('string-dedent')
 
 const pkg = require('./package.json')
-
 const version = process.env.SEMANTIC_RELEASE_NEXT_RELEASE_VERSION
 
 if (!version) {
   throw new Error(`Invalid version, got: "${version}".`)
 }
 
-const banner = dedent`
-  /*!
-   * ${pkg.name} v${version}
-   * ${pkg.homepage}
-   *
-   * Copyright (c) 2023 ${pkg.author}
-   * Released under the ${pkg.license} License
-   * ${pkg.homepage}/blob/main/LICENSE
-   *
-   * Date: ${new Date().toISOString()}
-   */
-`
-
 /** @type {import('rollup').RollupOptions} */
-module.exports = {
-  input: 'src/index.ts',
+module.exports = [
+  {
+    input: 'src/index.ts',
 
-  output: {
-    banner,
-    sourcemap: true,
+    output: {
+      format: 'cjs',
+      sourcemap: true,
+      preserveModules: true,
+      dir: 'dist'
+    },
 
-    format: 'cjs',
-    preserveModules: true,
+    external: /@grandom\/.*/,
 
-    dir: 'dist'
+    plugins: [
+      typescript()
+    ]
   },
+  {
+    input: 'src/index.ts',
 
-  plugins: [
-    typescript()
-    // terser(),
-  ]
-}
+    output: {
+      format: 'umd',
+      name: 'grandomWeighted',
+      sourcemap: true,
+      file: 'dist/umd.min.js',
+
+      banner: dedent`
+        /*!
+        * ${pkg.name} v${version}
+        * ${pkg.homepage}
+        *
+        * Copyright (c) ${new Date().getFullYear()} ${pkg.author}
+        * Released under the ${pkg.license} License
+        * ${pkg.homepage}/blob/main/LICENSE
+        *
+        * Date: ${new Date().toISOString()}
+        */
+      `
+    },
+
+    plugins: [
+      terser(),
+      typescript(),
+      commonjs(),
+      nodeResolve()
+    ]
+  }
+]
