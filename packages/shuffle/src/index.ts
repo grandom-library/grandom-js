a0aa8943abde7c673253e55d827ceb5389356206
Richard King <richrdkng@gmail.com>
2023-08-27 09:52:49 +0200

    fix(shuffle): make RandomShuffle accessible in UMD

--- packages/shuffle/src/index.ts
+++ packages/shuffle/src/index.ts
@@ -4,4 +4,8 @@ import RandomShuffle from './RandomShuffle'
 const random = new RandomShuffle(new BasicEngine())
 const shuffle = random.shuffle.bind(random)
 
+// @ts-expect-error
+// make RandomShuffle accessible in UMD
+random.RandomShuffle = RandomShuffle
+
 export default shuffle
