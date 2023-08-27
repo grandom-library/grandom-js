c6c7f0ac07a8355c51b082fc919246bdf0c89146
Richard King <richrdkng@gmail.com>
2023-08-27 09:47:35 +0200

    fix(pick): make RandomPick accessible in UMD build

--- packages/pick/src/index.ts
+++ packages/pick/src/index.ts
@@ -4,4 +4,8 @@ import RandomPick from './RandomPick'
 const random = new RandomPick(new BasicEngine())
 const pick = random.pick.bind(random)
 
+// @ts-expect-error
+// make RandomPick accessible in UMD
+random.RandomPick = RandomPick
+
 export default pick
