36ccd6da62eac7f379be482685934d6617d00a2f
Richard King <richrdkng@gmail.com>
2023-08-27 09:58:44 +0200

    fix(string): make RandomString accessible in UMD

--- packages/string/src/index.ts
+++ packages/string/src/index.ts
@@ -4,4 +4,8 @@ import RandomString from './RandomString'
 const random = new RandomString(new BasicEngine())
 const string = random.string.bind(random)
 
+// @ts-expect-error
+// make RandomString accessible in UMD
+random.RandomString = RandomString
+
 export default string
