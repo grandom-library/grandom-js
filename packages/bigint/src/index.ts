591a25fe86e740fa4ff0540335a0dd091043c30e
Richard King <richrdkng@gmail.com>
2023-08-28 17:46:00 +0200

    fix(bigint): remove unneeded UMD-related code

--- packages/bigint/src/index.ts
+++ packages/bigint/src/index.ts
@@ -4,13 +4,4 @@ import RandomBigInt from './RandomBigInt'
 const random = new RandomBigInt(new BasicEngine())
 const bigint = random.bigint.bind(random)
 
-/* <umd-only> */
-// Make internals accessible in UMD --------------------------------------------
-
-// @ts-expect-error
-bigint.RandomBigInt = RandomBigInt
-
-// -----------------------------------------------------------------------------
-/* </umd-only> */
-
 export default bigint
