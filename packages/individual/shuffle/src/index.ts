02471243359e1c75832ed2518b1e9a28af427e0c
Richard King <richrdkng@gmail.com>
2023-09-02 00:15:38 +0200

    fix(shuffle): remove unnecessary UMD-only code

--- packages/individual/shuffle/src/index.ts
+++ packages/individual/shuffle/src/index.ts
@@ -4,8 +4,4 @@ import RandomShuffle from './RandomShuffle'
 const random = new RandomShuffle(new BasicEngine())
 const shuffle = random.shuffle.bind(random)
 
-// @ts-expect-error
-// make RandomShuffle accessible in UMD
-random.RandomShuffle = RandomShuffle
-
 export default shuffle
