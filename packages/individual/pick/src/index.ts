6b6cf11058f4ed49e8c25bee5ae79067fe5a9364
Richard King <richrdkng@gmail.com>
2023-09-02 00:15:25 +0200

    fix(pick): remove unnecessary UMD-only code

--- packages/individual/pick/src/index.ts
+++ packages/individual/pick/src/index.ts
@@ -4,8 +4,4 @@ import RandomPick from './RandomPick'
 const random = new RandomPick(new BasicEngine())
 const pick = random.pick.bind(random)
 
-// @ts-expect-error
-// make RandomPick accessible in UMD
-random.RandomPick = RandomPick
-
 export default pick
