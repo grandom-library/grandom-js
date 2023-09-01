a147e28309d69447836a1b8e8aef460002628ead
Richard King <richrdkng@gmail.com>
2023-09-02 00:00:15 +0200

    fix(boolean): fix UMD export name typo

--- packages/individual/boolean/rollup.config.js
+++ packages/individual/boolean/rollup.config.js
@@ -2,5 +2,5 @@ const config = require('../../../shared/rollup.config')
 const pkg = require('./package.json')
 
 module.exports = [
-  ...config.getDefaultConfig(pkg, 'grandomBigInt')
+  ...config.getDefaultConfig(pkg, 'grandomBoolean')
 ]
