ea90b5f87b4876a659909097955cc13e8d0a68ba
Richard King <richrdkng@gmail.com>
2023-09-02 00:16:19 +0200

    fix(weighted): remove unnecessary UMD-only code

--- packages/individual/weighted/src/index.ts
+++ packages/individual/weighted/src/index.ts
@@ -4,8 +4,4 @@ import RandomWeighted from './RandomWeighted'
 const random = new RandomWeighted(new BasicEngine())
 const weighted = random.weighted.bind(random)
 
-// @ts-expect-error
-// make RandomWeighted accessible in UMD
-random.RandomWeighted = RandomWeighted
-
 export default weighted
