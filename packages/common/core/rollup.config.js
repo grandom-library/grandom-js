853be0ea9c2fc28c624502aa0a6546a8460893e9
Richard King <richrdkng@gmail.com>
2023-09-01 16:29:52 +0200

    build(core): use shared configs

--- packages/common/core/rollup.config.js
+++ packages/common/core/rollup.config.js
@@ -1,23 +1,3 @@
-const typescript = require('@rollup/plugin-typescript')
+const config = require('../../../shared/rollup.config')
 
-const version = process.env.SEMANTIC_RELEASE_NEXT_RELEASE_VERSION
-
-if (!version) {
-  throw new Error(`Invalid version, got: "${version}".`)
-}
-
-/** @type {import('rollup').RollupOptions} */
-module.exports = {
-  input: 'src/index.ts',
-
-  output: {
-    format: 'cjs',
-    sourcemap: true,
-    preserveModules: true,
-    dir: 'dist'
-  },
-
-  plugins: [
-    typescript()
-  ]
-}
+module.exports = config.distOnly()
