efa3e1a7003145cd61eb0ab3967ca6856b67b07e
Richard King <richrdkng@gmail.com>
2023-09-01 23:58:48 +0200

    build(boolean): use shared configs

--- packages/individual/boolean/release.config.js
+++ packages/individual/boolean/release.config.js
@@ -1,32 +1,5 @@
-/** @type {import('semantic-release').Options} */
+const config = require('../../../shared/release.config')
+
 module.exports = {
-  extends: 'semantic-release-monorepo',
-  branches: [
-    '+([0-9])?(.{+([0-9]),x}).x',
-    'main',
-    'next',
-    'next-major',
-    { name: 'beta', prerelease: true },
-    { name: 'alpha', prerelease: true }
-  ],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    '@semantic-release/github',
-    ['@semantic-release/exec', {
-      prepareCmd: 'export SEMANTIC_RELEASE_NEXT_RELEASE_VERSION=${nextRelease.version} && npm run build'
-    }],
-    ['@semantic-release/git', {
-      assets: [
-        'CHANGELOG*'
-      ]
-    }],
-    ['@semantic-release/npm', {
-      pkgRoot: 'dist'
-    }],
-    ['@semantic-release/npm', {
-      pkgRoot: 'dist-umd'
-    }]
-  ]
+  ...config.defaultConfig
 }
