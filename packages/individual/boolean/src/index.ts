e52f2ecf64c168dcf7d46b834c7093cae7f09d44
Richard King <richrdkng@gmail.com>
2023-09-02 00:13:17 +0200

    fix(boolean): remove unnecessary UMD-only code in src

--- packages/individual/boolean/src/index.ts
+++ packages/individual/boolean/src/index.ts
@@ -4,13 +4,4 @@ import RandomBoolean from './RandomBoolean'
 const random = new RandomBoolean(new BasicEngine())
 const boolean = random.boolean.bind(random)
 
-/* <umd-only> */
-// Make internals accessible in UMD --------------------------------------------
-
-// @ts-expect-error
-boolean.RandomBoolean = RandomBoolean
-
-// -----------------------------------------------------------------------------
-/* </umd-only> */
-
 export default boolean
