9525a3f0b467cab50758dc0ea9d805f6bbfed323
Richard King <richrdkng@gmail.com>
2023-09-02 00:35:24 +0200

    build(pick): use shared configs

--- packages/individual/pick/jest.config.js
+++ packages/individual/pick/jest.config.js
@@ -1,18 +1,5 @@
-/** @type {import('jest').Config} */
+const config = require('../../../shared/jest.config')
+
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ['jest-extended/all'],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
-    }
-  },
-  coverageReporters: [
-    'lcov',
-    'text'
-  ]
+  ...config
 }
