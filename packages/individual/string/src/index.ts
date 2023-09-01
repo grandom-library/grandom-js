6401f46c4b39cb313cda67830729cf43bfee310b
Richard King <richrdkng@gmail.com>
2023-09-02 00:16:08 +0200

    fix(string): remove unnecessary UMD-only code

--- packages/individual/string/src/index.ts
+++ packages/individual/string/src/index.ts
@@ -4,8 +4,4 @@ import RandomString from './RandomString'
 const random = new RandomString(new BasicEngine())
 const string = random.string.bind(random)
 
-// @ts-expect-error
-// make RandomString accessible in UMD
-random.RandomString = RandomString
-
 export default string
